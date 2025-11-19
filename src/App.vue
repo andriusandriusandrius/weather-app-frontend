c4b57cdf32a176bc0cf42e6e984481e46c9fa4ea
andriusandriusandrius <and.zurba@gmail.com>
2025-11-19 21:16:41 +0200

    add weather forecast card

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherForecast from "./components/WeatherForecast.vue";
+import sunIcon from "./assets/weather/clear-day.svg";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <WeatherForecast
+    city="siauliai"
+    :icon="sunIcon"
+    :temperature="15"
+    :humidity="12"
+    :wind="3"
+    :pressure="3"
+    sunrise="06:15"
+    sunset="19:15"
+  />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/WeatherForecast.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card has-text-centered" style="max-width: 250px">
+    <div class="card-content">
+      <p class="title">{{ city }}</p>
+
+      <p class="my-3 is-size-5">
+        <strong>{{ temperature }}°C</strong>
+      </p>
+
+      <div class="content">
+        <div class="is-flex is-justify-content-space-between is-size-6">
+          <div>
+            <p>Humidity: {{ humidity }}%</p>
+            <p>Wind Speed: {{ wind }} km/h</p>
+            <figure class="image is-16x16 is-inline-block">
+              <img clas="is-rounded" :src="icon" alt="weather icon" />
+            </figure>
+          </div>
+
+          <div class="is-size-7">
+            <p>Pressure: {{ pressure }} hPa</p>
+            <p>Sunrise: {{ sunrise }}</p>
+            <p>Sunset: {{ sunset }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type WeatherForecastProps = {
+  city: string;
+  icon: string;
+  temperature: number;
+  humidity: number;
+  wind: number;
+  pressure: number;
+  sunrise: string;
+  sunset: string;
+};
+defineProps<WeatherForecastProps>();
+</script>
