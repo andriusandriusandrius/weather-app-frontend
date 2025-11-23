8f9ed6eee20fecb8b7f1ba753fb46146ef0c9c7e
andriusandriusandrius <and.zurba@gmail.com>
2025-11-23 15:20:55 +0200

    forecasts display

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,15 @@ import Layout from "./components/Layout.vue";
 import { ref } from "vue";
 
 import AddForecastModal from "./components/AddForecastModal.vue";
+import type { WeatherData } from "./types/WeatherData";
+import WeatherForecast from "./components/WeatherForecast.vue";
 
 const showModal = ref(false);
+const forecasts = ref<WeatherData[]>([]);
 
+function addForecast(data: WeatherData) {
+  forecasts.value.push(data);
+}
 function openModal() {
   showModal.value = true;
 }
@@ -15,8 +21,12 @@ function openModal() {
 <template>
   <Layout title="weather.io">
     <AddForecastButton @click="openModal" />
+    <div class="is-flex is-flex-direction-column" style="gap: 1rem">
+      <WeatherForecast v-for="(f, i) in forecasts" :key="i" :forecast="f" />
+    </div>
     <AddForecastModal
       v-if="showModal"
+      @add="addForecast"
       @close="showModal = false"
     ></AddForecastModal>
   </Layout>
